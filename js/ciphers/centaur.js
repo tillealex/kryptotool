11c29fd9946ce939d858e0b69cd13ee3369e4cb4
tillealex <tillealex@gmail.com>
2026-06-22 23:22:22 +0200

    refactor: rebuild first centaur glyphs with svg primitives

--- js/ciphers/centaur.js
+++ js/ciphers/centaur.js
@@ -3,43 +3,68 @@ window.KryptoTool.ciphers = window.KryptoTool.ciphers || {};
 
 (function registerCentaurCipher(ns) {
   const makeSvg = (label, body) => `
-    <svg class="centaur-svg" viewBox="0 0 80 80" role="img" aria-label="Zentaurisch ${label}" xmlns="http://www.w3.org/2000/svg">
-      <g fill="currentColor" stroke="currentColor" stroke-width="8" stroke-linecap="square" stroke-linejoin="miter">
+    <svg class="centaur-svg" viewBox="0 0 100 100" role="img" aria-label="Zentaurisch ${label}" xmlns="http://www.w3.org/2000/svg">
+      <g fill="currentColor" stroke="none">
         ${body}
       </g>
     </svg>`;
 
-  const line = (x1, y1, x2, y2) => `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}"/>`;
-  const poly = (points) => `<polygon points="${points}"/>`;
+  const round = (value) => Number.parseFloat(value.toFixed(2));
+
+  const barH = (x, y, length = 56, thickness = 10, chamfer = 6) => {
+    const x2 = x + length;
+    const y2 = y + thickness;
+    return `<polygon points="${x + chamfer},${y} ${x2 - chamfer},${y} ${x2},${y + thickness / 2} ${x2 - chamfer},${y2} ${x + chamfer},${y2} ${x},${y + thickness / 2}"/>`;
+  };
+
+  const barV = (x, y, length = 58, thickness = 10, chamfer = 6) => {
+    const x2 = x + thickness;
+    const y2 = y + length;
+    return `<polygon points="${x + thickness / 2},${y} ${x2},${y + chamfer} ${x2},${y2 - chamfer} ${x + thickness / 2},${y2} ${x},${y2 - chamfer} ${x},${y + chamfer}"/>`;
+  };
+
+  const barDiag = (x1, y1, x2, y2, thickness = 10) => {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    const length = Math.hypot(dx, dy);
+    const nx = (-dy / length) * (thickness / 2);
+    const ny = (dx / length) * (thickness / 2);
+
+    return `<polygon points="${round(x1 + nx)},${round(y1 + ny)} ${round(x2 + nx)},${round(y2 + ny)} ${round(x2 - nx)},${round(y2 - ny)} ${round(x1 - nx)},${round(y1 - ny)}"/>`;
+  };
+
+  const triDown = (cx, cy, size = 10) => `<polygon points="${cx - size},${cy - size / 2} ${cx + size},${cy - size / 2} ${cx},${cy + size / 2}"/>`;
+  const triLeft = (cx, cy, size = 10) => `<polygon points="${cx - size / 2},${cy} ${cx + size / 2},${cy - size} ${cx + size / 2},${cy + size}"/>`;
+  const triRight = (cx, cy, size = 10) => `<polygon points="${cx + size / 2},${cy} ${cx - size / 2},${cy - size} ${cx - size / 2},${cy + size}"/>`;
 
   const GLYPHS = {
-    A: makeSvg("A", `${line(14,54,36,30)}${line(44,30,66,54)}${line(24,54,56,54)}`),
-    B: makeSvg("B", `${line(18,22,62,22)}${line(18,58,62,58)}${poly("31,35 49,35 40,47")}`),
-    C: makeSvg("C", `${line(30,20,30,60)}${poly("46,28 58,40 46,52")}`),
-    D: makeSvg("D", `${line(18,24,52,24)}${line(18,40,52,40)}${line(18,56,52,56)}${line(58,22,58,58)}${poly("18,28 30,40 18,52")}`),
-    E: makeSvg("E", `${line(18,30,62,30)}${line(18,50,62,50)}`),
-    F: makeSvg("F", `${line(30,20,30,60)}${poly("48,28 60,40 48,52")}`),
-    G: makeSvg("G", `${line(18,24,58,24)}${line(18,56,58,56)}${poly("54,34 66,46 54,58")}`),
-    H: makeSvg("H", `${line(30,20,30,60)}${poly("48,24 58,34 48,44")}`),
-    I: makeSvg("I", `${line(40,18,40,62)}`),
-    J: makeSvg("J", `${line(48,20,48,58)}${line(38,58,48,58)}${poly("24,48 36,58 36,38")}`),
-    K: makeSvg("K", `${line(34,40,58,18)}${line(34,40,58,62)}${line(24,40,34,40)}`),
-    L: makeSvg("L", `${line(32,20,32,60)}${line(32,60,46,60)}${poly("48,52 60,62 48,72")}`),
-    M: makeSvg("M", `${line(24,20,24,60)}${line(40,20,40,60)}${line(56,20,56,60)}`),
-    N: makeSvg("N", `${line(20,26,60,26)}${line(40,26,24,54)}${line(40,26,56,54)}`),
-    O: makeSvg("O", `${line(18,28,32,52)}${line(62,28,48,52)}${line(26,28,54,28)}${line(32,52,48,52)}`),
-    P: makeSvg("P", `${line(32,20,32,60)}${poly("50,28 62,40 50,52")}`),
-    Q: makeSvg("Q", `${line(20,28,60,28)}${line(24,48,56,48)}${poly("28,48 36,60 44,48")}${poly("52,48 60,60 68,48")}`),
-    R: makeSvg("R", `${line(28,20,28,60)}${line(28,22,58,22)}${line(28,40,48,40)}`),
-    S: makeSvg("S", `${line(48,20,48,60)}${line(32,60,48,60)}${poly("22,50 34,60 34,40")}${poly("52,22 64,34 52,46")}`),
-    T: makeSvg("T", `${line(40,20,40,62)}${poly("24,28 36,40 24,52")}${poly("56,28 44,40 56,52")}`),
-    U: makeSvg("U", `${line(26,20,26,58)}${line(54,20,54,58)}${line(26,58,54,58)}`),
-    V: makeSvg("V", `${line(20,28,60,28)}${poly("31,42 49,42 40,56")}`),
-    W: makeSvg("W", `${line(18,30,34,54)}${line(62,30,46,54)}${line(34,54,46,54)}${poly("34,30 46,30 40,42")}`),
-    X: makeSvg("X", `${line(22,24,58,56)}${line(58,24,22,56)}`),
-    Y: makeSvg("Y", `${line(24,56,58,24)}${poly("20,28 30,20 40,30")}`),
-    Z: makeSvg("Z", `${line(22,24,58,24)}${line(58,24,22,56)}${line(22,56,58,56)}`),
-    "-": makeSvg("Bindestrich", `${line(22,36,60,36)}${poly("58,26 70,36 58,46")}${poly("16,42 28,52 16,62")}`),
+    A: makeSvg("A", `${barDiag(20,68,42,34)}${barDiag(58,34,80,68)}${barH(30,64,40,10)}`),
+    B: makeSvg("B", `${barH(22,26,56,10)}${barH(22,60,56,10)}${triDown(50,47,9)}`),
+    C: makeSvg("C", `${barV(30,20,40,10)}${triRight(52,28,10)}${triRight(52,52,10)}`),
+    D: makeSvg("D", `${barH(18,24,44,10)}${barH(18,40,44,10)}${barH(18,56,44,10)}${barV(58,22,42,10)}${triRight(22,40,10)}`),
+    E: makeSvg("E", `${barH(22,28,56,10)}${barH(22,62,56,10)}`),
+    F: makeSvg("F", `${barV(30,20,40,10)}${triRight(52,30,10)}${triRight(52,50,10)}`),
+    G: makeSvg("G", `${barH(18,24,50,10)}${barH(18,56,50,10)}${triLeft(66,46,10)}`),
+    H: makeSvg("H", `${barV(36,20,52,10)}${triRight(58,34,9)}`),
+    I: makeSvg("I", `${barV(45,20,60,10)}`),
+    J: makeSvg("J", `${barV(48,20,42,10)}${triLeft(30,58,9)}`),
+    K: makeSvg("K", `${barDiag(32,40,58,18)}${barDiag(32,40,58,62)}${barH(22,35,14,10)}`),
+    L: makeSvg("L", `${barV(32,20,48,10)}${triRight(52,62,9)}`),
+    M: makeSvg("M", `${barV(24,20,42,10)}${barV(40,20,42,10)}${barV(56,20,42,10)}`),
+    N: makeSvg("N", `${barH(22,24,56,10)}${barDiag(28,62,50,34)}${barDiag(72,62,50,34)}`),
+    O: makeSvg("O", `${barH(26,26,48,10)}${barDiag(22,34,40,62)}${barDiag(78,34,60,62)}${barH(40,62,20,10)}`),
+    P: makeSvg("P", `${barV(32,20,48,10)}${triRight(56,40,10)}`),
+    Q: makeSvg("Q", `${barH(20,28,60,10)}${barH(24,48,48,10)}${triDown(36,62,8)}${triDown(60,62,8)}`),
+    R: makeSvg("R", `${barV(28,20,48,10)}${barH(28,22,34,10)}`),
+    S: makeSvg("S", `${barV(44,22,56,10)}${triLeft(35,64,8)}${triRight(64,34,8)}`),
+    T: makeSvg("T", `${barV(40,20,56,10)}${triLeft(30,40,9)}${triRight(58,40,9)}`),
+    U: makeSvg("U", `${barV(26,20,44,10)}${barV(54,20,44,10)}${barH(32,60,22,10)}`),
+    V: makeSvg("V", `${barH(22,30,56,10)}${triDown(50,52,10)}`),
+    W: makeSvg("W", `${barDiag(20,34,40,62)}${barDiag(80,34,60,62)}${barH(40,62,20,10)}${triDown(50,44,8)}`),
+    X: makeSvg("X", `${barDiag(24,24,58,56)}${barDiag(58,24,24,56)}`),
+    Y: makeSvg("Y", `${barDiag(28,60,62,26)}${triUp(30,28,8)}`),
+    Z: makeSvg("Z", `${barH(22,24,40,10)}${barDiag(62,24,28,60)}${barH(28,60,40,10)}`),
+    "-": makeSvg("Bindestrich", `${barH(24,36,48,10)}${triLeft(74,36,9)}${triRight(18,56,9)}`),
   };
 
   const ALPHABET = Object.keys(GLYPHS);
